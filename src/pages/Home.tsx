09747c1f75788ccdbdfbac020d9b4fa654c94ba7
Sean Wong <seanwong24@live.com>
2021-08-03 02:54:02 -0600

    add a calendar view in home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,26 @@
-import { IonButtons, IonContent, IonHeader, IonMenuButton, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonButtons, IonCard, IonContent, IonHeader, IonMenuButton, IonPage, IonSelect, IonSelectOption, IonTitle, IonToolbar } from '@ionic/react';
+import Calendar from '@toast-ui/react-calendar';
+import { useState } from 'react';
+import 'tui-calendar/dist/tui-calendar.css';
+import 'tui-date-picker/dist/tui-date-picker.css';
+import 'tui-time-picker/dist/tui-time-picker.css';
 
-import './Home.css';
+import styles from './Home.module.css';
 
 const Home: React.FC = () => {
+  const [calendarView, setCalendarView] = useState('week');
+  const [isShowingCalendar, setIsShowingCalendar] = useState(false);
+
+  // TODO this is a ugly solution to fix calender height
+  const resizeObsserver = new ResizeObserver(entries => {
+    entries.forEach(entry => {
+      setIsShowingCalendar(false);
+      setTimeout(() => {
+        setIsShowingCalendar(true);
+      });
+    });
+  });
+
   return (
     <IonPage>
       <IonHeader>
@@ -11,15 +29,38 @@ const Home: React.FC = () => {
             <IonMenuButton />
           </IonButtons>
           <IonTitle>Home</IonTitle>
+          <IonSelect
+            slot="end"
+            interface="popover"
+            value={calendarView}
+            onIonChange={({ detail }) => setCalendarView(detail.value)}
+          >
+            {
+              ['month', 'week', 'day'].map(view => (
+                <IonSelectOption value={view}>
+                  {view[0].toUpperCase() + view.substring(1)}
+                </IonSelectOption>
+              ))
+            }
+          </IonSelect>
         </IonToolbar>
       </IonHeader>
 
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Home</IonTitle>
-          </IonToolbar>
-        </IonHeader>
+      <IonContent scrollY={false}>
+        <IonCard
+          className={styles['calender-container']}
+          ref={el => resizeObsserver.observe(el as any)}
+        >
+          {
+            isShowingCalendar &&
+            <Calendar
+              height="100%"
+              view={calendarView}
+              useDetailPopup
+              useCreationPopup
+            ></Calendar>
+          }
+        </IonCard>
       </IonContent>
     </IonPage>
   );
